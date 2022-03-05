bfc3576806407b6c460c6c9de6f0eceb5e216342
noellecrow <crownoelle@gmail.com>
2022-03-05 08:21:21 -0800

    employee class files

--- employee.test.js
+++ employee.test.js
@@ -1,4 +1,24 @@
 const Employee = require('./Employee');
 const employee = new Employee('Noelle', '567890', 'crownoelle@gmail.com');
 
+test('test if we can retrieve constructor values for employee object', () => {
+    expect(employee.name).toBe('Noelle');
+    expect(employee.id).toBe('567890');
+    expect(employee.email).toBe('crownoelle@gmail.com');
+});
 
+test('test if we can retrieve name from getName() method', () => {
+    expect(employee.getName()).toBe('Noelle');
+});
+
+test('test if we can retrieve id from getId() method', () => {
+    expect(employee.getId()).toBe('567890');
+});
+
+test('test if we can retrieve email from getEmail() method', () => {
+    expect(employee.getEmail()).toBe('crownoelle@gmail.com');
+});
+
+test('test if we can retrieve role from getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
